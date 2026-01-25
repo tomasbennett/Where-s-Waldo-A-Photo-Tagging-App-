39b991c7a0826ddc3922bb433e813940c6897c4b
tomasbennett <tjs.bennett@gmail.com>
2026-01-25 07:29:34 +0000

    Receiving coordinates from clicking on main image

--- client/src/features/play/layouts/PlayLayout.tsx
+++ client/src/features/play/layouts/PlayLayout.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import styles from "./PlayLayout.module.css";
 import { Navigate, Outlet, useMatches, useNavigate } from "react-router-dom";
 import { IPlayContextHandle, PlayContextHandleSchema } from "../../../models/IPlayContextHandle";
@@ -74,13 +74,11 @@ export function PlayLayout() {
         if (imgNaturalWidth / imgNaturalHeight > 1) {
             // WIDE IMAGE
             setMainImgRatio("wide");
-            // mainImgRef.current.style.height = `100%`;
-            // mainImgRef.current.style.width = `auto`;
+
         } else {
             // TALL IMAGE
             setMainImgRatio("tall");
-            // mainImgRef.current.style.width = `100%`;
-            // mainImgRef.current.style.height = `auto`;
+
 
         }
         
@@ -111,6 +109,34 @@ export function PlayLayout() {
     // }, []);
 
 
+    const onClickImg = (e: React.MouseEvent<HTMLImageElement>) => {
+        console.log("Main image clicked!!!");
+
+        const mainImg = mainImgRef.current;
+        if (!mainImg) {
+            console.log("Main image ref is null, cannot get click coordinates!!!");
+            return;
+        }
+
+
+        const rect = mainImg.getBoundingClientRect();
+
+        const clickX = e.clientX - rect.left;
+        const clickY = e.clientY - rect.top;
+
+        const scaleX = mainImg.naturalWidth / rect.width;
+        const scaleY = mainImg.naturalHeight / rect.height;
+
+        const clickXPixels = Math.floor(clickX * scaleX);
+        const clickYPixels = Math.floor(clickY * scaleY);
+
+        console.log(`Click coordinates: (X: ${clickXPixels}, Y: ${clickYPixels})`);
+
+
+
+
+    }
+
 
 
     return (
@@ -137,7 +163,7 @@ export function PlayLayout() {
 
                 <div data-img-ratio={mainImgRatio} className={styles.mainImageContainer}>
 
-                    <img data-img-ratio={mainImgRatio} onLoad={() => {
+                    <img onClick={onClickImg} data-img-ratio={mainImgRatio} onLoad={() => {
                         calculateLayout();
                         // addResizeObserver();
                     }} ref={mainImgRef} src={playHandle.imgUrl} alt={`Main Image: ${playHandle.backendRoute}`} />
